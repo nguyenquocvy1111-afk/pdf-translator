39d5e50f3e108c2a9b30e63d7479845f0abab109
nguyenquocvy1111-afk <nguyenquocvy1111@gmail.com>
2026-04-05 22:29:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
-import streamlit as st
-from PyPDF2 import PdfReader
 from deep_translator import GoogleTranslator
 
-st.title("📄 PDF Translator (EN → VI)")
-
-uploaded_file = st.file_uploader("Upload PDF", type=["pdf"])
-
-
-if uploaded_file:
-    reader = PdfReader(uploaded_file)
-    text = ""
-
-    for page in reader.pages:
-        if page.extract_text():
-            text += page.extract_text() + "\n"
-
-    if st.button("Translate"):
-        translated = GoogleTranslator(source='en', target='vi').translate(text)
-        
-        st.text_area("Kết quả", translated, height=400)
-
-        st.download_button(
-            "Download",
-            translated,
-            file_name="translated.txt"
-        )
+def translate_text(text):
+    chunk_size = 4000  # nhỏ hơn 5000 cho chắc
+    chunks = [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
+    
+    translated_chunks = []
+    
+    for chunk in chunks:
+        try:
+            translated = GoogleTranslator(source='en', target='vi').translate(chunk)
+            translated_chunks.append(translated)
+        except:
+            translated_chunks.append("[Lỗi dịch đoạn này]")
+    
+    return "\n".join(translated_chunks)
+         
